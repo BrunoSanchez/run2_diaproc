8d7d32030e7ef400318f0042f4ca348285f5096d
Bruno Sanchez <bruno.sanchez@duke.edu>
2019-10-09 21:09:04 -0700

    adding an important notebook

--- obtain_visits.py
+++ obtain_visits.py
@@ -30,23 +30,28 @@ database = '/global/cscratch1/sd/desc/DC2/data/Run2.1i/rerun/calexp-v1/tracts_ma
 conn = sqlite3.connect(database)
 c = conn.cursor()
 
-query = "select DISTINCT(visit), filter from overlaps WHERE tract=4639 and patch='(0, 0)' order by visit"
+def main(tract, patchi, patchj, suffix):
+    query = "select DISTINCT(visit), filter from overlaps WHERE tract={} and patch='({}, {})' order by visit".format(tract, patchi, patchj)
 
-visitab = pd.read_sql_query(query, conn)
+    visitab = pd.read_sql_query(query, conn)
 
 
-cmd = """nice -n 10 coaddDriver.py /global/cscratch1/sd/desc/DC2/data/Run2.1i/rerun/calexp-v1 --output $SCRATCH/templates_003 --configfile $HOME/run2_diaproc/coadd_config_example.py  --id tract=4639 patch=0,0 filter={} --selectId visit={} --job templ_02  --cores 32 --time 400  --batch-type=smp #  --batch-verbose  --batch-stats --batch-options='-C knl -q regular' --mpiexec='-bind-to socket' # --dry-run --clobber-output 
-"""
-commands = []
-for filtr, visits in visitab.groupby('filter'):
-    visitstr = ''
-    for avisit in visits.visit:
-        visitstr+=str(avisit)+'^'
-        
-    commands.append(cmd.format(filtr, visitstr[:-1]))
+    cmd = """nice -n 10 coaddDriver.py /global/cscratch1/sd/bos0109/run2.1i_softln/rerun/calexp-v1 --output $SCRATCH/templates_004 --configfile $HOME/run2_diaproc/coadd_config_example.py  --id tract={} patch={},{} filter={} --selectId visit={} --job templ_03  --cores 4 --time 400  --batch-type=smp #  --batch-verbose  --batch-stats --batch-options='-C knl -q regular' --mpiexec='-bind-to socket' # --dry-run --clobber-output 
+    """
+    commands = []
+    for filtr, visits in visitab.groupby('filter'):
+        visitstr = ''
+        for avisit in visits.visit:
+            visitstr+=str(avisit)+'^'
+            
+        commands.append(cmd.format(tract, patchi, patchj, filtr, visitstr[:-1]))
 
-with open('coaddCommands.sh', 'w') as cf:
-    for acmd in commands:
-        cf.write(acmd)
-        cf.write('\n \n')
-        
+    with open('coaddCommands_{}.sh'.format(suffix), 'w') as cf:
+        for acmd in commands:
+            cf.write(acmd)
+            cf.write('\n \n')
+           
+
+if __name__=='__main__':
+    import sys
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
