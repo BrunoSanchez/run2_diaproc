d094e5dbe4695708a0734de9bd7a3ee2e953b50d
Bruno SANCHEZ <bruno.sanchez@duke.edu>
2019-10-21 11:52:57 -0400

    added the dia commands

--- create_diaCommands.py
+++ create_diaCommands.py
@@ -26,27 +26,57 @@
 import sqlite3
 import pandas as pd
 
+
 database = '/global/cscratch1/sd/desc/DC2/data/Run2.1i/rerun/calexp-v1/tracts_mapping.sqlite3'
-
-conn = sqlite3.connect(database)
-c = conn.cursor()
-
-query = "select DISTINCT(visit), filter from overlaps WHERE tract=4639 and patch='(0, 0)' order by visit"
-
-visitab = pd.read_sql_query(query, conn)
-
-cmd = "time nice -n 10 imageDifferenceDriver.py  /global/cscratch1/sd/bos0109/templates_003/rerun/multiband --output /global/cscratch1/sd/bos0109/test_imdiff_run2  --id visit={} -C imageDifferenceDriver_config.py --batch-type=smp --mpiexec='-bind-to socket'   --cores 4  --job imdiff_v{}_f{} --time 500 --batch-options='-C knl -q regular'"
+query_tmpl = "select DISTINCT(visit), filter from overlaps WHERE tract={} and patch={} order by visit"
+cmd = "time nice -n 10 imageDifferenceDriver.py  /global/cscratch1/sd/bos0109/templates_003/rerun/multiband \
+       --output /global/cscratch1/sd/bos0109/test_imdiff_run2  --id visit={} \
+       -C imageDifferenceDriver_config.py --batch-type={} --mpiexec='-bind-to socket'\
+       --cores {}  --job imdiff_v{}_f{} --time 500 --batch-options='-C knl -q regular'"
 
 
-commands = []
-for filtr, visits in visitab.groupby('filter'):
-    if filtr not in ['u','y']:
-        print(filtr, visits.visit)
-        for avisit in visits.visit:
-            commands.append(cmd.format(avisit, avisit, filtr))
+def main(tract, patch, filters='griz', 
+         outfile='driver_commands/diaCommands.sh', 
+         database=database, batch='smp', cores=4):
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
+    visitab = pd.read_sql_query(query.format(tract, str(patch)), conn)
+    commands = []
+    for filtr, visits in visitab.groupby('filter'):
+        if filtr in list(filters):
+            print(filtr, visits.visit)
+            for avisit in visits.visit:
+                commands.append(cmd.format(avisit, batch, cores, avisit, filtr))
 
-with open('diaCommands.sh', 'w') as cf:
-    for acmd in commands:
-        cf.write(acmd)
-        cf.write('\n \n')
-        
+    with open(outfile, 'w') as cf:
+        for acmd in commands:
+            cf.write(acmd)
+            cf.write('\n \n')
+    print("Wrote {} commands to {}".format(len(commands), outfile))
+    return
+
+
+
+if __name__=='__main__':
+    import sys
+    import argparse
+    DESC = """Creates commands for difference image driver in the dia_pipe context."""
+    EPIL = """This will produce a file output with the commands separated by one line."""
+    parser = argparse.ArgumentParser(description=DESC, epilog=EPIL)
+    parser.add_argument('-t', '--tract', metavar='t', type=int, 
+                        help='Tract number')
+    parser.add_argument('-p', '--patch', metavar='p', type=str, 
+                        help='Patch code number')
+    parser.add_argument('-f', '--filter', metavar='f', type=str, help='Filter name')
+    parser.add_argument('-o', '--outfile', metavar='o', type=str, help='Output filename with the commands')
+    parser.add_argument('-db', '--database', metavar='db', type=str, dest='database', 
+                        default=database, help='Database of tract+patchs to find out visits')
+    parser.add_argument('-c', '--cores', metavar='c', type=int, default=4, help='Number of cores')
+    parser.add_argument('-b', '--batch-type', metavar='b', type=str, default='smp', help='slurm or smp batch processing')
+
+    args = parser.parse_args()
+    print(args)
+    import ipdb; ipdb.set_trace()
+    main(args.tract, args.patch, filters=args.filter, 
+         outfile=args.outfile, database=args.database, 
+         batch=args.batch_type, cores=args.cores)
