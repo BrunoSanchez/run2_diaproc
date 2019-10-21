76d2b30bcf908e84cc1cc6f36e9ee575b3008781
Bruno Sanchez <bruno.sanchez@duke.edu>
2019-10-21 14:14:46 -0700

    fixed create commands for dia

--- create_diaCommands.py
+++ create_diaCommands.py
@@ -29,24 +29,35 @@ import pandas as pd
 
 database = '/global/cscratch1/sd/desc/DC2/data/Run2.1i/rerun/calexp-v1/tracts_mapping.sqlite3'
 query_tmpl = "select DISTINCT(visit), filter from overlaps WHERE tract={} and patch={} order by visit"
-cmd = "time nice -n 10 imageDifferenceDriver.py  /global/cscratch1/sd/bos0109/templates_003/rerun/multiband \
-       --output /global/cscratch1/sd/bos0109/test_imdiff_run2  --id visit={} \
-       -C imageDifferenceDriver_config.py --batch-type={} --mpiexec='-bind-to socket'\
-       --cores {}  --job imdiff_v{}_f{} --time 500 --batch-options='-C knl -q regular'"
+tmpl_repo = '/global/cscratch1/sd/bos0109/templates_003/rerun/multiband'
+output_repo = '/global/cscratch1/sd/bos0109/test_imdiff_run2'
+config_path = 'imageDifferenceDriver_config.py'
+
+cmd_tmpl = "time nice -n 10 imageDifferenceDriver.py  {} --output {} "
+cmd_tmpl +="--id visit={} -C {} --batch-type={} --mpiexec='-bind-to socket' " 
+cmd_tmpl +="--cores {} --job imdiff_v{}_f{} --time 500 "
+cmd_tmpl +="--batch-options='-C knl -q regular'"
 
 
-def main(tract, patch, filters='griz', 
-         outfile='driver_commands/diaCommands.sh', 
-         database=database, batch='smp', cores=4):
+def main(tract, patch, filters='griz', outfile='driver_commands/diaCommands.sh', 
+         database=database, batch='smp', cores=4, tmpl_repo=tmpl_repo, 
+         output_repo=output_repo, config_path=config_path):
+
     conn = sqlite3.connect(database)
     #c = conn.cursor()
-    visitab = pd.read_sql_query(query_tmpl.format(tract, str(patch)), conn)
+    patchx, _, patchy = patch 
+    strpatch = "'"+str((int(patchx), int(patchy)))+"'"
+    query = query_tmpl.format(tract, strpatch)
+    visitab = pd.read_sql_query(query, conn)
+
     commands = []
     for filtr, visits in visitab.groupby('filter'):
         if filtr in list(filters):
             print(filtr, visits.visit)
             for avisit in visits.visit:
-                commands.append(cmd.format(avisit, batch, cores, avisit, filtr))
+                cmd = cmd_tmpl.format(tmpl_repo, output_repo, 
+                    avisit, config_path, batch, cores, avisit, filtr)
+                commands.append(cmd)
 
     with open(outfile, 'w') as cf:
         for acmd in commands:
@@ -60,22 +71,35 @@ def main(tract, patch, filters='griz',
 if __name__=='__main__':
     import sys
     import argparse
-    DESC = """Creates commands for difference image driver in the dia_pipe context."""
-    EPIL = """This will produce a file output with the commands separated by one line."""
+    DESC = "Creates commands for difference image driver in the dia_pipe context"
+    EPIL = "This will produce a file output with the commands separated by one line"
     parser = argparse.ArgumentParser(description=DESC, epilog=EPIL)
     parser.add_argument('-t', '--tract', metavar='t', type=int, 
                         help='Tract number')
     parser.add_argument('-p', '--patch', metavar='p', type=str, 
                         help='Patch code number')
-    parser.add_argument('-f', '--filter', metavar='f', type=str, help='Filter name')
-    parser.add_argument('-o', '--outfile', metavar='o', type=str, help='Output filename with the commands')
-    parser.add_argument('-db', '--database', metavar='db', type=str, dest='database', 
-                        default=database, help='Database of tract+patchs to find out visits')
-    parser.add_argument('-c', '--cores', metavar='c', type=int, default=4, help='Number of cores')
-    parser.add_argument('-b', '--batch-type', metavar='b', type=str, default='smp', help='slurm or smp batch processing')
+    parser.add_argument('-f', '--filter', metavar='f', type=str, 
+                        help='Filter name', default='griz')
+    parser.add_argument('-o', '--outfile', metavar='o', type=str, 
+                        help='Output filename with the commands', 
+                        default='driver_commands/diaCommands.sh')
+    parser.add_argument('-db', '--database', metavar='db', type=str, 
+                        dest='database', default=database, 
+                        help='Database of tract+patchs to find out visits')
+    parser.add_argument('-c', '--cores', metavar='c', type=int, default=4, 
+                        help='Number of cores')
+    parser.add_argument('-b', '--batch-type', metavar='b', type=str, 
+                        default='smp', help='slurm or smp batch processing')
+    parser.add_argument('-r', '--tmpl', metavar='R', type=str, default=tmpl_repo, 
+                        help='Repository where templates are located')
+    parser.add_argument('-d', '--diff', metavar='D', type=str, default=output_repo, 
+                        help='Repository where differences are going to be located')
+    parser.add_argument('-C', '--conf', metavar='C', type=str, default=config_path, 
+                        help='Path of configuration file')
+
 
     args = parser.parse_args()
-    print(args)
-    main(args.tract, args.patch, filters=args.filter, 
-         outfile=args.outfile, database=args.database, 
-         batch=args.batch_type, cores=args.cores)
+
+    main(args.tract, args.patch, filters=args.filter, outfile=args.outfile, 
+         database=args.database, batch=args.batch_type, cores=args.cores, 
+         tmpl_repo=args.tmpl, output_repo=args.diff, config_path=args.conf)
